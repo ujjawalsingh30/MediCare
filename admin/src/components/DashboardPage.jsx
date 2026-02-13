7565882bb6d16f88c8d856b9a4364f87b2282bc4
ujjawalsingh30 <ujjawalsingh8589@gmail.com>
2026-02-13 16:46:09 +0530

    Working on dashboard page

---
+++ admin/src/components/DashboardPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { dashboardStyles as s } from '../assets/dummyStyles';
+
+const API_BASE = 'http://localhost:4000';
+const PATIENT_COUNT_API = `${API_BASE}/api/appointments/patients/count`;
+
+//Hekper Function
+
+const DashboardPage = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default DashboardPage
