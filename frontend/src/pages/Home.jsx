e639e2a4fc42f811d4e07aec9f5488fcaae6ae50
ujjawalsingh30 <ujjawalsingh8589@gmail.com>
2026-02-23 22:13:21 +0530

    Testimonial section done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
 import Certification from '../components/Certification'
 import HomeDoctors from '../components/HomeDoctors'
+import Testimonial from '../components/Testimonial'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Banner/>
       <Certification/>
       <HomeDoctors/>
+      <Testimonial/>
     </div>
   )
 }
