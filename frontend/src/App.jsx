526e1af4949fb6a6e250d73b173c277a40af2098
ujjawalsingh30 <ujjawalsingh8589@gmail.com>
2026-02-23 23:54:00 +0530

    Creating DoctorsPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
+import Doctors from './pages/Doctors'
 
 const App = () => {
   return (
     <div>
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path='/doctors' element={<Doctors />} />
     </Routes>
   
     </div>
---
+++ frontend/src/pages/Doctors.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import DoctorsPage from '../components/DoctorsPage'
+
+const Doctors = () => {
+  return (
+    <div>
+      
+<Navbar/>
+<DoctorsPage/>
+<Footer/>
+
+    </div>
+  )
+}
+
+export default Doctors
