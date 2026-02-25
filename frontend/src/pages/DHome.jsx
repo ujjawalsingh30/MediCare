125c0e7c332d91e5fb2a2794b599394871c102d3
ujjawalsingh30 <ujjawalsingh8589@gmail.com>
2026-02-26 01:34:06 +0530

    Creating Doctors DashboardPge

--- frontend/src/pages/DHome.jsx
+++ frontend/src/pages/DHome.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import DoctorNavbar from '../doctor/DoctorNavbar'
+import DahboardPage from '../doctor/DashboardPage'
 
 
 const DHome = () => {
   return (
     <div>
      <DoctorNavbar/>
+     <DahboardPage/>
 
     </div>
   )
