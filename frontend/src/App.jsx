4f684c453febbfa292a0c0ea89fb00c7d9d5ba2a
ujjawalsingh30 <ujjawalsingh8589@gmail.com>
2026-02-25 16:49:02 +0530

    Creating the ServicePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,18 @@ import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
 import Doctors from './pages/Doctors'
 import DoctorDetail from './pages/DoctorDetail'
+import Service from './pages/Service'
 
 const App = () => {
   return (
     <div>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path='/doctors' element={<Doctors />} />
-      <Route path='/doctors/:id' element={<DoctorDetail/>} />
-    </Routes>
-  
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:id' element={<DoctorDetail />} />
+        <Route path='/services' element={<Service />} />
+      </Routes>
+
     </div>
   )
 }
---
+++ frontend/src/pages/Service.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import ServicePage from '../components/ServicePage'
+
+const Service = () => {
+    return (
+        <div>
+            <Navbar />
+            <ServicePage />
+            <Footer />
+        </div>
+    )
+}
+
+export default Service
