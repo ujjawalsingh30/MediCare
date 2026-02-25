f4bdbba2d4ea02ebdc5acb6a5ba5f5e09002cea6
ujjawalsingh30 <ujjawalsingh8589@gmail.com>
2026-02-25 16:38:38 +0530

    DoctorDetails page is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
 import Doctors from './pages/Doctors'
+import DoctorDetail from './pages/DoctorDetail'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path='/doctors' element={<Doctors />} />
+      <Route path='/doctors/:id' element={<DoctorDetail/>} />
     </Routes>
   
     </div>
